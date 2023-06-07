2b7f70ec160c2a2d1846aa5104cbdcbc1938a879
abhiram18b <83966296+abhiram18b@users.noreply.github.com>
2023-06-07 15:41:07 +0530

    Added solution - LeetHub

---
+++ Strong Numbers - GFG/strong-numbers.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.isStrong(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int isStrong(int N) {
+        // code here
+        int temp=N;
+        int sum=0;
+        
+        while(temp>0)
+        {
+            int d=temp%10;
+           
+            int product=1;
+            for(int i=1;i<=d;i++)
+            {
+                product*=i;
+            }
+            temp=temp/10;
+            sum+=product;
+        }
+        
+        if(sum==N) return 1;
+        else return 0;
+    }
+
+};
